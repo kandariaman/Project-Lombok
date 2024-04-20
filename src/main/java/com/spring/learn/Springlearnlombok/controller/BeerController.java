b8586c88e6cd9e29d93f1d35397600a97958588b
Aman Kandari <kandariaman24@gmail.com>
2024-04-20 14:02:27 +0530

    Added controller mapping for List of beer and get beer by Id

--- src/main/java/com/spring/learn/Springlearnlombok/controller/BeerController.java
+++ src/main/java/com/spring/learn/Springlearnlombok/controller/BeerController.java
@@ -2,23 +2,38 @@ package com.spring.learn.Springlearnlombok.controller;
 
 import com.spring.learn.Springlearnlombok.model.Beer;
 import com.spring.learn.Springlearnlombok.service.BeerService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@Controller
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
+    @Autowired
     public BeerController(BeerService beerService) {
         this.beerService = beerService;
     }
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable UUID beerId) {
 
         log.debug("Get Beer by id - in controller");
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Beer> getBeerList() {
+        return beerService.listBeers();
     }
 }
--- src/main/java/com/spring/learn/Springlearnlombok/service/BeerServiceImpl.java
+++ src/main/java/com/spring/learn/Springlearnlombok/service/BeerServiceImpl.java
@@ -6,22 +6,64 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.UUID;
+import java.util.*;
 
-@Slf4j
+
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
-    @Override
-    public Beer getBeerById(UUID id) {
 
-        log.debug("Get Beer Id in service was called");
-        return Beer.builder()
-                .id(id)
+    private Map<UUID, Beer> beerMap;
+
+    public BeerServiceImpl() {
+
+        beerMap = new HashMap<>();
+
+        Beer beer1 = Beer.builder()
+                .id(UUID.randomUUID())
                 .version(1)
                 .beerName("Galaxy Cat")
                 .beerStyle(BeerStyle.PALE_ALE)
                 .upc("123456")
                 .price(new BigDecimal("12.99"))
-                .quantityOnHand(122).build();
+                .quantityOnHand(122)
+                .build();
+
+        Beer beer2 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Beera")
+                .beerStyle(BeerStyle.LAGER)
+                .upc("123456")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .build();
+
+        Beer beer3 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Budweiser")
+                .beerStyle(BeerStyle.SAISON)
+                .upc("123456")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .build();
+
+        beerMap.put(beer1.getId(),beer1);
+        beerMap.put(beer2.getId(), beer2);
+        beerMap.put(beer3.getId(), beer3);
+    }
+
+
+    @Override
+    public List<Beer> listBeers() {
+        return new ArrayList<>(beerMap.values());
+    }
+
+    @Override
+    public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer Id in service was called");
+        return beerMap.get(id);
     }
 }
