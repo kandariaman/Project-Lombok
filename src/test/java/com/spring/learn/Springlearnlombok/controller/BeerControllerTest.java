d1548642f113dbb94de3ca717bc762352c45e7ad
Aman Kandari <kandariaman24@gmail.com>
2024-04-23 19:15:46 +0530

    Added a simple mock test

--- src/test/java/com/spring/learn/Springlearnlombok/controller/BeerControllerTest.java
+++ src/test/java/com/spring/learn/Springlearnlombok/controller/BeerControllerTest.java
@@ -1,21 +1,37 @@
 package com.spring.learn.Springlearnlombok.controller;
 
+import com.spring.learn.Springlearnlombok.service.BeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
-    @Autowired
-    BeerController beerController;
-    @Test
-    void getBeerById() {
+//    @Autowired
+//    BeerController beerController;
 
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
+
+    @Test
+    void getBeerById() throws Exception {
+
+        mockMvc.perform(get("api/v1/beer/" + UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
     }
 }
